7450c10e1d99b08479219a8d3a12585015ccac7f
jaimeGB17 <jaime.gonbra@educa.jcyl.es>
2024-11-26 09:00:29 +0100

    subido lo de angel (vistas)

---
+++ src/main/java/application/Controller/IniProfController.java
@@ -0,0 +1,45 @@
+package application.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class IniProfController {
+
+    @FXML
+    private AnchorPane AreaCrear;
+
+    @FXML
+    void OnCrearParteClic(MouseEvent event) {
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setContentText("Pantalla Crear Parte");
+        alerta.show();
+    }
+
+    @FXML
+    void OnMouseEnter(MouseEvent event) {
+        cambiarFondoAzul(AreaCrear);
+    }
+
+    @FXML
+    void OnMouseExit(MouseEvent event) {
+        cambiarFondoGris(AreaCrear);
+    }
+
+    @FXML
+    public void OnVolverClic(ActionEvent actionEvent) {
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setContentText("Ir a InicioSesion.fxml");
+        alerta.show();
+    }
+
+    public void cambiarFondoAzul(AnchorPane areaAzul){
+        areaAzul.setStyle("-fx-background-color: #A3CEEF;");
+    }
+
+    public void cambiarFondoGris(AnchorPane areaGris){
+        areaGris.setStyle("-fx-background-color: #E4E4E5;");
+    }
+}
--- src/main/java/application/Controller/LoginController.java
+++
@@ -1,4 +0,0 @@
-package application.Controller;
-
-public class LoginController {
-}
