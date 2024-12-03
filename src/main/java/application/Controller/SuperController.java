6ff47406ed07b530254d2ccd5e696381182edb3e
jaime.gonbra <jaime.gonbra@educa.jcyl.es>
2024-12-03 13:03:13 +0100

    a ver si me hace el commit

--- src/main/java/application/Controller/SuperController.java
+++ src/main/java/application/Controller/SuperController.java
@@ -1,5 +1,7 @@
 package application.Controller;
 
+import application.Model.Alumnos;
+import application.Model.Partes_incidencia;
 import application.Model.Profesores;
 
 public abstract class SuperController {
@@ -12,5 +14,25 @@ public abstract class SuperController {
 
     public void setProfesor(Profesores profe) {
         profesor = profe;
+    }
+
+    static Alumnos alumno = null;
+
+    public static Alumnos getAlumno() {
+        return alumno;
+    }
+
+    public static void setAlumno(Alumnos alumno) {
+        SuperController.alumno = alumno;
+    }
+
+    static Partes_incidencia parte = null;
+
+    public static Partes_incidencia getParte() {
+        return parte;
+    }
+
+    public static void setParte(Partes_incidencia parte) {
+        SuperController.parte = parte;
     }
 }
--- src/main/java/application/DAO/AlumnoDAO.java
+++ src/main/java/application/DAO/AlumnoDAO.java
@@ -4,19 +4,25 @@ import application.Model.Alumnos;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlumnoDAO extends ConexionDAO {
     // SessionFactory factory = getFactory();
     Session session = getSession();
 
-    public void listarAlumnos() {
+    public List<Alumnos> listarAlumnos() {
+        List<Alumnos> alumnos = new ArrayList<>();
         try {
             session.beginTransaction();
-            session.createQuery("from Alumnos", Alumnos.class);
+            alumnos = session.createQuery("from Alumnos", Alumnos.class)
+                    .stream().toList();
             session.getTransaction().commit();
         } catch (Exception e) {
             session.getTransaction().rollback();
         } finally {
             session.clear();
         }
+        return alumnos;
     }
 }
